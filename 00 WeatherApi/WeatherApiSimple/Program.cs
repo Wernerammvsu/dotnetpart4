92b70234b3b012eefec735e96d3f9aa3722f305f
Artem Makhno <artem_mahno@mail.ru>
2021-11-30 20:58:14 +0300

    Initial commit

---
+++ 00 WeatherApi/WeatherApiSimple/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace WeatherApiSimple
+{
+	public class Program
+	{
+		public static async Task<int> Main(string[] argv)
+		{
+			const string city = "Voronezh";
+			const string apiKey = "7b6be55ecfc023f52792505653e8e278";
+			const string template = "https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}";
+
+			var client = new HttpClient();
+			string url = string.Format(template, city, apiKey);
+			HttpResponseMessage result = await client.GetAsync(url);
+
+			Console.WriteLine(await result.Content.ReadAsStringAsync());
+
+			return 0;
+		}
+	}
+}
