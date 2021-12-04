c689d5bd7b6caac259375e3fcf94d721463b0b0d
Artem Makhno <artem_mahno@mail.ru>
2021-12-04 23:37:30 +0300

    Add redis

---
+++ 01 WeatherClientWeb/WeatherClientWeb/Controllers/WeatherController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using WeatherClientWeb.OpenWeather;
+
+namespace WeatherClientWeb.Controllers
+{
+	[Route("weather")]
+	public class WeatherController : Controller
+	{
+		public async Task<ActionResult> Get([Required, FromQuery(Name = "city")]string cityName)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest("Name of a city is not provided");
+
+			var openWeatherClient = new OpenWeather.OpenWeatherClient();
+
+			CurrentWeatherDto currentWeatherDto = await openWeatherClient.GetWeatherAsync(cityName);
+
+			return Ok(currentWeatherDto);
+		}
+	}
+}
